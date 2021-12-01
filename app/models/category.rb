4d3aab3c0de031d03373f864c6fb7369b8231df6
Sylvestre <pasytchangwa@gmail.com>
2021-12-01 20:10:19 +0200

    Add validation for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,12 @@
 class Category < ApplicationRecord
   belongs_to :user
+
+  has_and_belongs_to_many :entities
+
+  validates :name, presence: true, length: { maximum: 150 }
+  validates :icon, presence: true
+
+  def amount
+    entities.sum('amount')
+  end
 end
