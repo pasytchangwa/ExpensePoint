b6a3c9c3ee51b376a2b5f9ca7666d8709ff3e971
Sylvestre <pasytchangwa@gmail.com>
2021-12-01 21:23:22 +0200

    Modify entities controller

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,2 +1,29 @@
 class EntitiesController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    redirect_to categories_url
+  end
+
+  def new
+    @entity = Entity.new
+  end
+
+  def create
+    @entity = Entity.new(entity_params)
+    @entity.user = current_user
+
+    respond_to do |f|
+      if entity.save
+        f.html { redirect_to @entity.categories.first }
+      end
+    end
+  end
+
+  private
+
+  def entity_params
+    params.require(:entity).permit(:name :amount, category_ids: [])
+  end
 end
+
