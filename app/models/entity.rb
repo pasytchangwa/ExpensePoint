79ef70ac9c1d3cef70cd26427578a3e73eb154de
Sylvestre <pasytchangwa@gmail.com>
2021-12-01 20:10:39 +0200

    Add validation for entity

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,8 @@
 class Entity < ApplicationRecord
   belongs_to :user
+
+  has_and_belongs_to_many :categories
+
+  validates :name, :amount, :categories, presence: true
+  validates :amount, numericality: { greater_than: 0 }
 end
