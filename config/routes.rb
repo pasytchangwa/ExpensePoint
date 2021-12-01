0649c5c7b59b6556bf14b1d56ac7b1530f3a1baf
Sylvestre <pasytchangwa@gmail.com>
2021-12-01 20:11:18 +0200

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root to: 'categories#index', as: :authenticated_root
+  end
+
+  root to: "splash#index"
+
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+
+
+  resources :entities
+  resources :categories
 end
