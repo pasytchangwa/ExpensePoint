db77ad765abda40e718a216972e5336b56d0cb86
Sylvestre <pasytchangwa@gmail.com>
2021-12-02 12:03:12 +0200

    Modify entity model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,6 +3,6 @@ class Entity < ApplicationRecord
 
   has_and_belongs_to_many :categories
 
-  validates :name, :amount, :categories, presence: true
+  validates :name, :amount, :categories, presence: true, length: { maximum: 150 }
   validates :amount, numericality: { greater_than: 0 }
 end
